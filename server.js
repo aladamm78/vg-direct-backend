4b78df0329db9e0ad47788360cc7631cda45f951
aladamm78 <aladamm78@gmail.com>
2024-12-10 07:00:58 -0500

    render updates

--- server.js
+++ server.js
@@ -144,7 +144,4 @@ app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
