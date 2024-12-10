394b0c433c51c12857e6b5fc0114c8db37ab2f45
aladamm78 <aladamm78@gmail.com>
2024-12-10 06:54:01 -0500

    render updates

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ const reviewsRoutes = require('./routes/reviews');
 const searchRoutes = require("./routes/search");
 
 const app = express();
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const BASE_URL = "https://api.rawg.io/api";
 const API_KEY = "9aa05b2ff77b476c8ff49505059dd4ed";
