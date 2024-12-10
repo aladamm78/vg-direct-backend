a3020804d96b1151a350616fc342c7625fa518b8
aladamm78 <aladamm78@gmail.com>
2024-12-10 06:58:03 -0500

    render port fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const searchRoutes = require("./routes/search");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
