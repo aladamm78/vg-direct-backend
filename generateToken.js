253daf91dcb3f64c03136f53b047b3f03468a999
aladamm78 <156132081+aladamm78@users.noreply.github.com>
2024-12-11 18:54:15 -0500

    Update generateToken.js

--- generateToken.js
+++ generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // Replace this with your actual secret key used in the backend
-const SECRET_KEY = process.env.SECRET_KEY || 'b17b092199f992ad177f27d8a2ab3f2567a1f975a49dcb5a42c2ad1f798ee057e6742a6f6a742fea85ec5cbead950ce9ac65a1e0935ad76256f1e61055f9ebb8';
+const SECRET_KEY = process.env.SECRET_KEY || 'b17b...';
 
 // Payload for the token
 // const payload = {
